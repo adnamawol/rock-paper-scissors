fad1aa0ddacd55d7872e470ab841e2e49088c3f6
Amanda Low <lowpt.a@gmail.com>
2022-03-20 17:42:27 +0800

    Add simple UI to a game of SPS between user and computer which announces a winner when the first side scores 5

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scissors-paper-stone
+
+A project completed based on TOP's curriculum. 
--- script.js
+++ script.js
@@ -1,64 +1,67 @@
+const container = document.querySelector('#container');
+const results = document.querySelector('#results');
+const para = document.createElement('p');
 
-function playerPlay () {
-    let playerPreChoice = prompt("Please select your weapon: Scissors, Paper, or Stone!");
-    let playerChoice = playerPreChoice.toLowerCase();
-    return playerChoice;
-}
+
+const scissorsBtn = document.querySelector('#scissors');
+const paperBtn = document.querySelector('#paper');
+const stoneBtn = document.querySelector('#stone');
+
+
+scissorsBtn.addEventListener('click', () => playRound("scissors",computerPlay()));
+paperBtn.addEventListener('click',() => playRound("paper",computerPlay()));
+stoneBtn.addEventListener('click', () => playRound("stone",computerPlay()));
+
 
 
 function computerPlay () {
     let spsChoice = ["scissors", "paper", "stone"];
     let computerChoice = (spsChoice[Math.floor(Math.random()*spsChoice.length)]);
-    alert(computerChoice);
+    console.log(computerChoice);
     return computerChoice;
 }
 
+let compCumScore = 0;
+let playerCumScore = 0;
 
 function playRound (playerSelection, computerSelection) {
-    
-
     if (computerSelection === "scissors" && playerSelection === "paper") {
-
-        alert("You lose, scissors beats paper!");
-
+        para.textContent="The Machine gains 1 point, and you gain nothing.";
+        container.appendChild(para);
+        compCumScore += 1;
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
-
-        alert("You win, scissors beats paper!");
-
+        para.textContent="You gain 1 point, and the Machine gains nothing.";
+        container.appendChild(para);
+        playerCumScore += 1;
     } else if (computerSelection === "scissors" && playerSelection === "stone") {
-
-        alert("You win, stone beats scissors!");
-
+        para.textContent="You gain 1 point, and the Machine gains nothing.";
+        container.appendChild(para);
+        playerCumScore += 1;
     } else if (computerSelection === "stone" && playerSelection === "scissors") {
-
-        alert("You lose, stone beats scissors!");
-
+        para.textContent="The Machine gains 1 point, and you gain nothing.";
+        container.appendChild(para);
+        compCumScore += 1;
     } else if (computerSelection === "paper" && playerSelection === "stone") {
-
-        alert("You lose, paper beats stone!");
-
+        para.textContent="The Machine gains 1 point, and you gain nothing.";
+        container.appendChild(para);
+        compCumScore += 1;
     } else if (computerSelection === "stone" && playerSelection === "paper") {
-
-        alert("You win, paper beats stone!");
-        
+        para.textContent="You gain 1 point, and the Machine gains nothing.";
+        container.appendChild(para);
+        playerCumScore += 1;
     } else {
-        alert("It's a tie!")
+        para.textContent="It's a tie! No one gets a point! Tough luck.";
+        container.appendChild(para);
     } 
+    console.log(compCumScore, playerCumScore);
+    results.textContent=`The Machine: ${compCumScore} and You: ${playerCumScore}`;
 
+    if (compCumScore === 5) {
+        alert("Sorry, you lost to the Machine!");
+        return;
+    } else if (playerCumScore === 5) {
+        alert("Congratulations, you beat the Machine!");
+        return;
+    }
 }
 
-
-let computerScore;
-let playerScore;
-
-
-
-playRound(playerPlay(),computerPlay());
-
-
-
-
-
-/* if (playerSelection !== "scissors" || playerSelection !== "paper" || playerSelection !== "stone") {
-    prompt("Please enter only one of these three weapons","Scissors, Paper or Stone")
-} */
