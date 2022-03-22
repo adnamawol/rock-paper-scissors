5b9667ff7c5cfb8bdc5d326cb78dd823fad59b0b
Amanda Low <lowpt.a@gmail.com>
2022-03-22 16:28:41 +0800

    Update HTML document and Javascript to include new sections to display game info and outcomes to player

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # scissors-paper-stone
 
 A project completed based on TOP's curriculum. 
+
+Next to do on 21st March:
+1. Find out how to reset to 0 for all after one side scores 5
+2. Create a simple brand style guide to inform the UI design of SPS
+
+22nd March:
+
+Work out conditional statements to show if it's a tie, or just show both dead face maybe?
--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const container = document.querySelector('#container');
 const results = document.querySelector('#results');
+const playerCumResult = document.querySelector('#player-result');
+const compCumResult = document.querySelector('#comp-result');
 const para = document.createElement('p');
+const playerResult = document.createElement('p');
+const compResult = document.querySelector('p');
+const resultPlayerPic = document.createElement('img');
+const resultCompPic = document.createElement('img');
+
+
+const player = document.querySelector('#player');
+const comp = document.querySelector('#comp')
 
 
 const scissorsBtn = document.querySelector('#scissors');
 const paperBtn = document.querySelector('#paper');
 const stoneBtn = document.querySelector('#stone');
+
 
 
 scissorsBtn.addEventListener('click', () => playRound("scissors",computerPlay()));
@@ -28,40 +39,124 @@ function playRound (playerSelection, computerSelection) {
     if (computerSelection === "scissors" && playerSelection === "paper") {
         para.textContent="The Machine gains 1 point, and you gain nothing.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/hand.png';
+        player.appendChild(resultPlayerPic); 
+
+        resultCompPic.src='./photos/victory.png';
+        comp.appendChild(resultCompPic);
+
+
         compCumScore += 1;
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
         para.textContent="You gain 1 point, and the Machine gains nothing.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/victory.png';
+        player.appendChild(resultPlayerPic);
+
+        resultCompPic.src='./photos/hand.png';
+        comp.appendChild(resultCompPic);
+
         playerCumScore += 1;
     } else if (computerSelection === "scissors" && playerSelection === "stone") {
         para.textContent="You gain 1 point, and the Machine gains nothing.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/rock.png';
+        player.appendChild(resultPlayerPic);
+
+        resultCompPic.src='./photos/victory.png';
+        comp.appendChild(resultCompPic);
+
         playerCumScore += 1;
     } else if (computerSelection === "stone" && playerSelection === "scissors") {
         para.textContent="The Machine gains 1 point, and you gain nothing.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/victory.png';
+        player.appendChild(resultPlayerPic);
+
+        resultCompPic.src='./photos/rock.png';
+        comp.appendChild(resultCompPic);
+
         compCumScore += 1;
     } else if (computerSelection === "paper" && playerSelection === "stone") {
         para.textContent="The Machine gains 1 point, and you gain nothing.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/rock.png';
+        player.appendChild(resultPlayerPic);
+
+        resultCompPic.src='./photos/hand.png';
+        comp.appendChild(resultCompPic);
+
         compCumScore += 1;
     } else if (computerSelection === "stone" && playerSelection === "paper") {
         para.textContent="You gain 1 point, and the Machine gains nothing.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/victory.png';
+        player.appendChild(resultPlayerPic);
+
+        resultCompPic.src='./photos/rock.png';
+        comp.appendChild(resultCompPic);
+
         playerCumScore += 1;
     } else {
         para.textContent="It's a tie! No one gets a point! Tough luck.";
         container.appendChild(para);
+
+        resultPlayerPic.src='./photos/dead.png';
+        player.appendChild(resultPlayerPic);
+
+        resultCompPic.src='./photos/dead.png';
+        comp.appendChild(resultCompPic);
+
     } 
-    console.log(compCumScore, playerCumScore);
-    results.textContent=`The Machine: ${compCumScore} and You: ${playerCumScore}`;
+    playerCumResult.textContent=`You: ${playerCumScore}`;
+    compCumResult.textContent=`The Machine: ${compCumScore}`;
 
     if (compCumScore === 5) {
         alert("Sorry, you lost to the Machine!");
+        reset();
         return;
+        
     } else if (playerCumScore === 5) {
         alert("Congratulations, you beat the Machine!");
+        reset();
         return;
     }
 }
+
+const restartBtn = document.querySelector('#restart');
+restartBtn.addEventListener('click', () => reset())
+
+function reset () {
+    compCumScore = 0;
+    playerCumScore = 0;
+    playerCumResult.textContent=`You: ${playerCumScore}`;
+    compCumResult.textContent=`The Machine: ${compCumScore}`;
+    player.removeChild(resultPlayerPic);
+    comp.removeChild(resultCompPic);
+    container.removeChild(para);
+}
+
+
+const audioScissors = document.querySelector('.scissors-audio');
+const audioPaper = document.querySelector('.paper-audio');
+const audioStone = document.querySelector('.stone-audio')
+
+scissorsBtn.addEventListener('mouseover', () => {
+    audioScissors.play();
+    audioScissors.currentTime = 0;
+})
+paperBtn.addEventListener('mouseover', () => {
+    audioPaper.play();
+    audioPaper.currentTime = 0;
+})
+stoneBtn.addEventListener('mouseover', () => {
+    audioStone.play();
+    audioStone.currentTime = 0;
+} )
 
